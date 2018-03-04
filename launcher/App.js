c5ee58a9680a8a8ff1076570b7d29f8f31d802d6
Zili Yin <viztor@outlook.com>
2018-03-04 02:06:07 -0800

    Get Started

--- launcher/App.js
+++ launcher/App.js
@@ -1,13 +1,41 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, SafeAreaView, View, Image, TouchableOpacity } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <View style={styles.center}>
+          <Text style={styles.title}>Grow Together!</Text>
+        </View>
+        <View style={styles.imgContainer}>
+          <Image
+            style={{
+              position:'absolute',
+              width: '100%',
+              resizeMode: 'cover',
+              maxHeight: 40,
+              bottom: 0,
+            }}
+            source={require('./img/hill.png')}
+          />
+          <Image
+            resizeMode='contain'
+            style={{
+              maxHeight: 320,
+            }}
+            source={require('./img/kangaroo.png')}
+          />
+        </View>
+        <View style={styles.btnContainer}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={this.onPress}
+        >
+          <Text style={{color: 'white', fontWeight: 'bold'}}>Get Started</Text>
+        </TouchableOpacity>
+          <Text style={styles.ftn}>By continuing, you agree to our <Text style={{color: 'black'}}>Terms of Use & Privacy Policy</Text></Text>
+        </View>
       </View>
     );
   }
@@ -17,7 +45,49 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    alignItems: 'stretch',
+    justifyContent: 'center',
+  },
+  center: {
+    flex: 1,
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  imgContainer: {
+    flex: 2,
+    maxHeight: 300,
+    flexDirection: 'column-reverse',
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    zIndex: 99,
+  },
+  btnContainer: {
+    flex: 1,
+    backgroundColor: '#D9F1F5',
+    alignItems: 'stretch',
+    justifyContent: 'center',
+  },
+  title: {
+    position: 'absolute',
+    top: 80,
+    fontSize: 24,
+  },
+  ftn: {
+    position: 'absolute',
+    bottom: 30,
+    right: 0,
+    left: 0,
+    fontSize: 10,
+    textAlign: 'center',
+    color: 'grey'
+  },
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#43BCD3',
+    padding: 10,
+    margin: 40,
+    borderRadius: 20
   },
 });
