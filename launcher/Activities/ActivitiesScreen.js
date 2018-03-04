13b12bf5fa47fc2bcfa8d490e39d39643f79993b
Zili Yin <viztor@outlook.com>
2018-03-04 04:55:59 -0800

    Use asyncStorage as cache

--- launcher/Activities/ActivitiesScreen.js
+++ launcher/Activities/ActivitiesScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View, FlatList, Button, StyleSheet, ActivityIndicator } from 'react-native';
+import { Text, View, FlatList, Button, StyleSheet, ActivityIndicator, AsyncStorage } from 'react-native';
 import { ActivityItem } from './Components/Item'
 const dataUrl = "https://plgaia-staging.herokuapp.com/api/v1/post_get_active/4Wa0y74X1mAKKIo2qgiWii"
 
@@ -13,8 +13,16 @@ export class ActivitiesScreen extends React.Component {
     super(props)
     this.state = { isLoading: true }
   }
-  componentDidMount() {
-    return fetch(dataUrl, {
+  async componentDidMount() {
+    try {
+      const storedData = await AsyncStorage.getItem('@PL:activities')
+      this.setState({
+        isLoading: false,
+        dataSource: JSON.parse(storedData)
+      })
+    } catch (e) {
+    }
+    fetch(dataUrl, {
       headers: {
         'Authorization': 'Token token=ZVKgYbjoOxoM9fvuhDvQOAtt',
         'Content-Type': 'application/json'
@@ -22,6 +30,7 @@ export class ActivitiesScreen extends React.Component {
     })
     .then((response) => response.json())
     .then((responseJson) => {
+      AsyncStorage.setItem('@PL:activities', JSON.stringify(responseJson.get_active))
       this.setState({
         isLoading: false,
         dataSource: responseJson.get_active
