8b86a51dd4707af39e6a869deb2dee25a9a84e7f
Zili Yin <viztor@outlook.com>
2018-03-04 03:07:01 -0800

    Add navigation

--- launcher/App.js
+++ launcher/App.js
@@ -1,93 +1,15 @@
 import React from 'react';
-import { StyleSheet, Text, SafeAreaView, View, Image, TouchableOpacity } from 'react-native';
+import { StackNavigator } from 'react-navigation';
+import { Welcome } from './Welcome';
+import { Activities } from './Activities'
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <View style={styles.center}>
-          <Text style={styles.title}>Grow Together!</Text>
-        </View>
-        <View style={styles.imgContainer}>
-          <Image
-            style={{
-              position:'absolute',
-              width: '100%',
-              resizeMode: 'cover',
-              maxHeight: 40,
-              bottom: 0,
-            }}
-            source={require('./img/hill.png')}
-          />
-          <Image
-            resizeMode='contain'
-            style={{
-              maxHeight: 320,
-            }}
-            source={require('./img/kangaroo.png')}
-          />
-        </View>
-        <View style={styles.btnContainer}>
-        <TouchableOpacity
-          style={styles.button}
-          onPress={this.onPress}
-        >
-          <Text style={{color: 'white', fontWeight: 'bold'}}>Get Started</Text>
-        </TouchableOpacity>
-          <Text style={styles.ftn}>By continuing, you agree to our <Text style={{color: 'black'}}>Terms of Use & Privacy Policy</Text></Text>
-        </View>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'stretch',
-    justifyContent: 'center',
+const App = StackNavigator({
+  Home: {
+    screen: Welcome,
   },
-  center: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  imgContainer: {
-    flex: 2,
-    maxHeight: 300,
-    flexDirection: 'column-reverse',
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    zIndex: 99,
-  },
-  btnContainer: {
-    flex: 1,
-    backgroundColor: '#D9F1F5',
-    alignItems: 'stretch',
-    justifyContent: 'center',
-  },
-  title: {
-    position: 'absolute',
-    top: 80,
-    fontSize: 24,
-  },
-  ftn: {
-    position: 'absolute',
-    bottom: 30,
-    right: 0,
-    left: 0,
-    fontSize: 10,
-    textAlign: 'center',
-    color: 'grey'
-  },
-  button: {
-    alignItems: 'center',
-    backgroundColor: '#43BCD3',
-    padding: 10,
-    margin: 40,
-    borderRadius: 20
+  Activities: {
+    screen: Activities,
   },
 });
+
+export default App;
